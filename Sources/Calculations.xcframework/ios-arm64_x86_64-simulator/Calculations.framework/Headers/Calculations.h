46394e55cd7303f3743529950a1ccb58a56b97b6
nidhi_experiment <nidhi@narendar-sbis-MacBook-Pro.local>
2023-10-20 09:56:49 +0530

    initial commit

---
+++ Sources/Calculations.xcframework/ios-arm64_x86_64-simulator/Calculations.framework/Headers/Calculations.h
@@ -0,0 +1,18 @@
+//
+//  Calculations.h
+//  Calculations
+//
+//  Created by nidhi_experiment on 20/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Calculations.
+FOUNDATION_EXPORT double CalculationsVersionNumber;
+
+//! Project version string for Calculations.
+FOUNDATION_EXPORT const unsigned char CalculationsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Calculations/PublicHeader.h>
+
+

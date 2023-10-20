2f6f55203b1fac5c448364d923859133d0fb8220
nidhi_experiment <nidhi@narendar-sbis-MacBook-Pro.local>
2023-10-20 12:12:53 +0530

    package modified

--- Package.swift
+++ Package.swift
@@ -19,6 +19,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         
+        .target(name: "cal_test_spm",
+                dependencies: [
+                    .target(name: "Calculations"),
+                    .product(name: "GreetSPM", package: "GreetSPM"),
+               ]),
         .binaryTarget(name: "Calculations", path: "./Sources/Calculations.xcframework")
     ]
 )
---
+++ Sources/cal_test_spm/cal_test_spm.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by nidhi_experiment on 20/10/23.
+//
+
+import Foundation
